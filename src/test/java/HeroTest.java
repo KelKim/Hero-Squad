d2e809d186d667fd8063e4e1213e5af024079ce9
Kelvins <neguskel@gmail.com>
2019-02-20 16:54:37 +0300

    added tests

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -5,8 +5,32 @@ public class HeroTest {
 
   @Test
   public void Hero_instantiatesCorrectly_true() {
-    Hero myHero = new Hero("Superman", 32, "Super strength", "");
+    Hero myHero = new Hero("Superman", 32, "Super strength", "ladies");
     assertEquals(true, myHero instanceof Hero);
+  }
+
+   @Test 
+  public void Hero_instantiatesWithName_String(){
+    Hero myHero = new Hero("Spiderman",17,"fly","ladies");
+    assertEquals("Spiderman",myHero.getName());
+  }
+​
+  @Test 
+  public void Hero_instantiatesWithAge_Int(){
+    Hero myHero = new Hero("Spiderman",17,"fly","ladies");
+    assertEquals(30, myHero.getAge());
+  }
+​
+  @Test 
+  public void Hero_instantiatesWithPower_String(){
+    Hero myHero = new Hero("Spiderman",17,"fly","ladies");
+    assertEquals("fly",myHero.getPower());
+  }
+​
+  @Test 
+  public void Hero_instantiatesWithWeakness_String(){
+    Hero myHero = new Hero("Spiderman",17,"fly","ladies");
+    assertEquals("slow",myHero.getWeakness());
   }
 
 }
